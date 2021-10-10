f173506724b8c44299605142945110de94e9e747
Heriberto lopez <alexito2002.ll@gmail.com>
2021-10-10 16:23:51 -0600

    updated server

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const path = require("path");
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
@@ -15,18 +13,15 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
-
-// routes
-app.use(require("./routes/api.js"));
+app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
