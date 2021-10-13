2c885fd507964e2c3e9530a325e9cb4fc255e099
Heriberto lopez <alexito2002.ll@gmail.com>
2021-10-13 13:51:32 -0600

    I think this should do it

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -15,12 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
 });
-
 // routes
 app.use(require("./routes/api.js"));
 
